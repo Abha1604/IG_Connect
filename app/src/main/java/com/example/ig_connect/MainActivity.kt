a27fb99707f0b9517234662152bd935037f8be2b
Abha1604 <abhaofficial1604@gmail.com>
2025-03-24 20:39:24 +0530

    Homepage 1st edit

--- app/src/main/java/com/example/ig_connect/MainActivity.kt
+++ app/src/main/java/com/example/ig_connect/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.ig_connect
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
-}
+    }
